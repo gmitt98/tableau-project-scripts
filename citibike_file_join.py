ee04538c991aa7f19fa1386fb7d7946de088c95e
Galen <gmitt98@users.noreply.github.com>
2023-07-24 15:47:00 -0700

    add comments to code

--- citibike_file_join.py
+++ citibike_file_join.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import os
 
+# move to the right directory
 current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir((current_directory))
 
-#print my current directory to check...
+# print my current directory to check...
 print("Current Directory:", current_directory)
 
+# list the files and set up and empty dataframe
 csv_files = ["JC-202303-citibike-tripdata.csv", "JC-202304-citibike-tripdata.csv", "JC-202305-citibike-tripdata.csv", "JC-202306-citibike-tripdata.csv"]
 dataframes = []
 
+# open the files and append to my dataframe
 for file in csv_files:
     df = pd.read_csv(file)
     dataframes.append(df)
 
 combined_df = pd.concat(dataframes, ignore_index=True)
 
+# save it
 output_csv = "citibike_data_mar-jun_combined.csv"
 
 combined_df.to_csv(output_csv, index=False)
--- station_volume_ranked.py
+++ station_volume_ranked.py
@@ -1,27 +1,32 @@
 import pandas as pd
 import os
 
+# move to current directory and open combined file
 current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir((current_directory))
 
 file_path = 'citibike_data_mar-jun_combined.csv'
 df = pd.read_csv(file_path)
 
+# group the data and count things
 grouped_data = df.groupby('start_station_id').agg({
     'start_station_id': 'count',
     'start_lat': 'mean',
     'start_lng': 'mean'
 })
 
+# rename columns so it's clear what i did
 grouped_data.rename(columns={
     'start_station_id': 'count_of_rides',
     'start_lat': 'avg_start_lat',
     'start_lon': 'avg_start_lng'
 }, inplace=True)
 
+# add rankings
 sorted_data = grouped_data.sort_values(by='count_of_rides', ascending=False)
 sorted_data['ranking'] = range(1, len(sorted_data) + 1)
 
+# save it off
 output_file_path = 'stations_volume_ranked.csv'
 sorted_data.to_csv(output_file_path)
 
