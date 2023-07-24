27ed0e46883c085452d896b66f01224ab4bfbab4
Galen <gmitt98@users.noreply.github.com>
2023-07-24 13:33:35 -0700

    add station volume ranking script and output

---
+++ station_volume_ranked.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir((current_directory))
+
+file_path = 'citibike_data_mar-jun_combined.csv'
+df = pd.read_csv(file_path)
+
+grouped_data = df.groupby('start_station_id').agg({
+    'start_station_id': 'count',
+    'start_lat': 'mean',
+    'end_lat': 'mean'
+})
+
+grouped_data.rename(columns={
+    'start_station_id': 'count_of_rides',
+    'start_lat': 'avg_start_lat',
+    'end_lat': 'avg_end_lat'
+}, inplace=True)
+
+sorted_data = grouped_data.sort_values(by='count_of_rides', ascending=False)
+sorted_data['ranking'] = range(1, len(sorted_data) + 1)
+
+output_file_path = 'stations_volume_ranked.csv'
+sorted_data.to_csv(output_file_path)
+
+print(f"successfully saved new file to {output_file_path}")
